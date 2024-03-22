5b0c22acf2530bdf9d6a753c2c511cf042a7a129
Chourkybob <charlesetiennetardif@hotmail.com>
2024-03-21 23:35:08 -0500

    modif acceleration main.c

--- driver_motor/src/main.cpp
+++ driver_motor/src/main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
+#include "AccelStepper.h"
 
 #define GPIO_ENABLE GPIO_NUM_25
 #define GPIO_DIR GPIO_NUM_32
-#define GPIO_STEP GPIO_NUM_32
+#define GPIO_STEP GPIO_NUM_33
 
 #define GPIO_MS1 GPIO_NUM_26
 #define GPIO_MS2 GPIO_NUM_27
 
 uint64_t nb_steps = 200;
 uint8_t direction = 1;
+AccelStepper stepper(AccelStepper::DRIVER, GPIO_STEP, GPIO_DIR); // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+
 
 // put function declarations here:
 int myFunction(int, int);
@@ -19,6 +22,14 @@ void setup() {
 	pinMode(GPIO_STEP, OUTPUT);
 	pinMode(GPIO_MS1, OUTPUT);
 	pinMode(GPIO_MS2, OUTPUT);
+	// stepper.setMaxSpeed(1000);
+	// stepper.setSpeed(50);	
+
+	//stepper.setEnablePin(GPIO_ENABLE);
+    //stepper.enableOutputs();
+    stepper.setMaxSpeed(1000000);
+    stepper.setSpeed(100000);
+	stepper.setAcceleration(500);
 	// put your setup code here, to run once:
 	// int result = myFunction(2, 3);
 	// uint64_t nb_steps = 200;
@@ -29,23 +40,34 @@ void setup() {
 
 void loop() {
 	// put your main code here, to run repeatedly:
-	digitalWrite(GPIO_ENABLE, LOW);
-	digitalWrite(GPIO_DIR, direction);
-	for(uint64_t i = 0; i < nb_steps; i++)
-	{
-		if(i < nb_steps)
-		digitalWrite(GPIO_STEP, HIGH);
-		// gpio_set_level(GPIO_NUM_2, 1);
-		usleep(1000);
-		digitalWrite(GPIO_STEP, LOW);
-		// gpio_set_level(GPIO_NUM_2, 0);
-		usleep(1000);
-	}
-	digitalWrite(GPIO_ENABLE, HIGH);
-	usleep(1000000);
+	//stepper.runSpeed();
+	stepper.moveTo(1000000);
+  while (stepper.currentPosition() != 500000) // Full speed up to 300
+    stepper.run();
+  stepper.runToNewPosition(0); // Cause an overshoot then back to 0
+
+	// digitalWrite(GPIO_ENABLE, LOW);
+	// digitalWrite(GPIO_DIR, direction);
+	// for(uint64_t i = 0; i < nb_steps; i++)
+	// {
+	// 	if(i < nb_steps)
+	// 	digitalWrite(GPIO_STEP, HIGH);
+	// 	// gpio_set_level(GPIO_NUM_2, 1);
+	// 	usleep(1000);
+	// 	digitalWrite(GPIO_STEP, LOW);
+	// 	// gpio_set_level(GPIO_NUM_2, 0);
+	// 	usleep(1000);
+	// }
+	// digitalWrite(GPIO_ENABLE, HIGH);
+	//usleep(1000000);
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
 	return x + y;
 }
+
+// defines pins numbers
+// const int stepPin = 3;
+// const int directionPin = 4;
+// const int enablePin = 5;
