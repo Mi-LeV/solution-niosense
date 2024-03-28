c1902b4432091cd51da80e597ed5a7273370b979
MicheLeVeilleur <florian.stanilewicz@gmail.com>
2024-03-27 21:39:22 -0400

    change folder

--- feu-niosense/include/filesys.h
+++ feu-niosense/include/filesys.h
@@ -5,5 +5,6 @@
 #include <LittleFS.h>
 
 String ouvrir_fichier(String nom_fichier);
-
+void appendFile(const char * name_file, const char * message);
+void writeFile(const char * name_file, const char * message);
 #endif
