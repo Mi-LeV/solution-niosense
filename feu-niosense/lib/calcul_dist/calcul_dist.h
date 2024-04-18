399c8e8bec40a4a893e23d9097f55a1a0b9337b4
MicheLeVeilleur <florian.stanilewicz@gmail.com>
2024-04-18 17:40:35 -0400

    constantes calcul comm

--- feu-niosense/lib/calcul_dist/calcul_dist.h
+++ feu-niosense/lib/calcul_dist/calcul_dist.h
@@ -6,15 +6,11 @@
 #include "wifi_server.h"
 
 
-//////////////////////////////////////
-////////////////////        CONSTANTES A DEFINIR
-/////////////////////
-
 #define FEU_LATERAL_OFFSET (2.0f) // offset lateral des rails en cm 
-#define FEU_LONGITUDINAL_OFFSET (10.0f) // offset longitudinal des rails des extermites en cm
-#define DIST_TO_CM (0.01f) // constant of conversion of step distance to cm
+#define FEU_LONGITUDINAL_OFFSET (15.5f) // offset longitudinal des rails des extermites en cm
+#define DIST_TO_CM (0.00103333333f) // constant of conversion of step distance to cm
 
-#define RF_SIMUL_PROB_INTERF (0.01f) // proba entre 0 et 1 d'avoir une interference sur la communication simulee
+#define RF_SIMUL_PROB_INTERF (0.1f) // proba entre 0 et 1 d'avoir une interference sur la communication simulee
 
 bool compute_comm(long dist,float rf_simul_reach_cm,float rf_simul_prob_interf);
 void camionAtFeu(long dist);
