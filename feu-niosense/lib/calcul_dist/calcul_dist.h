80f20acae8b2b2b20cd27bbab31a3bef2f830caa
MicheLeVeilleur <florian.stanilewicz@gmail.com>
2024-04-19 12:05:11 -0400

    modif coonstantes longueur

--- feu-niosense/lib/calcul_dist/calcul_dist.h
+++ feu-niosense/lib/calcul_dist/calcul_dist.h
@@ -5,14 +5,14 @@
 #include <stdlib.h>
 #include "wifi_server.h"
 
-
-#define FEU_LATERAL_OFFSET (2.0f) // offset lateral des rails en cm 
-#define FEU_LONGITUDINAL_OFFSET (15.5f) // offset longitudinal des rails des extermites en cm
-#define DIST_TO_CM (0.00103333333f) // constant of conversion of step distance to cm
+#define AXE_LENGHT (21.7f)//leght of the rail in cm
+#define FEU_LATERAL_OFFSET (17.5f) // offset lateral des rails en cm 
+#define FEU_LONGITUDINAL_OFFSET (10.85f) // offset longitudinal des rails des extermites en cm
+#define DIST_TO_CM ((float)(AXE_LENGHT/32000)) // constant of conversion of step distance to cm
 
 #define RF_SIMUL_PROB_INTERF (10)
 // proba entre 0 et 100 d'avoir une interference sur la communication simulee
-#define RF_SIMUL_RANGE (3.0f)
+#define RF_SIMUL_RANGE (20.0f)
 //portee de la RF simulee en CM
 
 bool compute_comm(long dist);
